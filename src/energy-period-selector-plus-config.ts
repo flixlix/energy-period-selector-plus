66aa483e2ba50380dc652b5740cee26dd16d74fc
Luca Félix <luca.ziegler.felix@gmail.com>
2023-11-03 10:27:32 +0100

    feat: :sparkles: add option for `default_period`

--- src/energy-period-selector-plus-config.ts
+++ src/energy-period-selector-plus-config.ts
@@ -1,6 +1,8 @@
 import { LovelaceCardConfig } from 'custom-card-helpers';
 import { EnergyCardBaseConfig } from './type/energy-card-base-config';
 
+export type Period = 'day' | 'week' | 'month' | 'year' | 'custom';
+
 export interface EnergyPeriodSelectorPlusConfig extends LovelaceCardConfig, EnergyCardBaseConfig {
   card_background?: boolean;
   prev_next_buttons?: boolean;
@@ -9,4 +11,5 @@ export interface EnergyPeriodSelectorPlusConfig extends LovelaceCardConfig, Ener
   today_button_type?: string | boolean;
   period_buttons?: string[];
   custom_period_label?: string;
+  default_period?: Period;
 }
